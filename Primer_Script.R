c9ffeb79d19e1e0baecba2a7af9739805c18cd95
FlorAguilarP <91507142+FlorAguilarP@users.noreply.github.com>
2021-09-27 19:53:47 -0500

    Primer_Script
    Construí un vector que se llama cajita, lo visualicé y calculé la media aritmética.

---
+++ Primer_Script.R
@@ -0,0 +1,16 @@
+
+# Primer script. 
+#(R no lo va a leer todo lo que este despues #), para activar (Run)
+# Mi Primer vector
+
+cajita<-c(3,15,16,20,10)
+
+#visualizar cajita (no olvidar activar)
+cajita 
+
+#Como calcular la media (muestral)
+mean(cajita)
+
+
+
+
